9e3e512ccfb57117391dbe5e617f940f7baadf1b
iwokiwo <bayuiwo@outlook.co.id>
2025-06-05 22:40:02 +0800

    mobile view nav

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,15 +1,34 @@
-import { cn } from '@/lib/utils'
-import Link from 'next/link'
-import React, { ReactNode } from 'react'
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
 
-const Logo = ({className}: {className?: string}) => {
+const Logo = ({
+  className,
+  spanDesign,
+}: {
+  className?: string;
+  spanDesign?: string;
+}) => {
   return (
-    <Link href={"/"}>
-      <h2 className={cn("text-2xl text-shop_dark_green font-black tracking-wide uppercase hover:text-shop_light_green", className)}>
-        Ren<span>Car</span>
+    <Link href={"/"} className="inline-flex">
+      <h2
+        className={cn(
+          "text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans",
+          className
+        )}
+      >
+        RentCa
+        <span
+          className={cn(
+            "text-shop_light_green group-hover:text-shop_dark_green hoverEffect",
+            spanDesign
+          )}
+        >
+          r
+        </span>
       </h2>
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
