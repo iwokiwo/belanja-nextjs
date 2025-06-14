a102d54e3a9f0c748074d38ca361c0039cbfef03
iwokiwo <bayuiwo@outlook.co.id>
2025-06-14 13:29:27 +0800

    sanity

---
+++ app/(client)/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/Container"
+import HomeBanner from "@/components/HomeBanner"
+import { Button } from "@/components/ui/button"
+
+const Home=()=>{
+  return(
+
+    <Container>
+     <HomeBanner/>
+    </Container>
+
+  )
+}
+export default Home
