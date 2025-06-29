252aa656368ddebfa2536e2198360d438aa717db
iwokiwo <bayuiwo@outlook.co.id>
2025-06-29 12:44:28 +0800

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,26 +14,38 @@ const Header = async() => {
   const user = await currentUser()
 
   return (
-    <header className='bg-white py-5 border-b border-b-black/20'>
-      <Container className='flex items-center justify-between'>
+    <header className="sticky top-0 z-50 py-5 bg-white/70 backdrop-blur-md">
+      <Container className="flex items-center justify-between text-lightColor">
         <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
-            <MobileMenu />
-            <Logo />
+          <MobileMenu />
+          <Logo />
         </div>
         <HeaderMenu />
-        <div className='w-auto md:w-1/3 flex items-center justify-end gap-5'>
-          <SerachBar/>
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+          {/* <SearchBar /> */}
           <CartIcon />
           <FavoriteButton />
+
+          {/* {user && (
+            <Link
+              href={"/orders"}
+              className="group relative hover:text-shop_light_green hoverEffect"
+            >
+              <Logs />
+              <span className="absolute -top-1 -right-1 bg-shop_btn_dark_green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
+                {orders?.length ? orders?.length : 0}
+              </span>
+            </Link>
+          )} */}
+
           <ClerkLoaded>
             <SignedIn>
-              <UserButton/>
+              <UserButton />
             </SignedIn>
-          {!user && <SignIn />}
+            {!user && <SignIn />}
           </ClerkLoaded>
         </div>
       </Container>
-      
     </header>
   )
 }
